aa0d60dd4ac5fd4d48bcc93ee7173899acc5ea9c
abhay15999 <abhaychadha15@gmail.com>
2023-08-11 21:59:52 -0400

    home page and contact page

--- portfolio/src/app/components/home/home.component.ts
+++ portfolio/src/app/components/home/home.component.ts
@@ -1,20 +1,56 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
-import { RouterModule } from '@angular/router';
-
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule, HeaderComponent, FooterComponent, RouterModule],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-  image = 'assets/ReplaceSelf.png';
-  Name= "Richa Chadha"
-  Descripton = "Full Stack Developer"
-  Content = "HTML/CSS5, JavaScript, TypeScript, NodeJS, ExpressJS, ReactJS, Angular, MongoDB"
+export class HomeComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    this.initTypewriter();
+  }
+
+  initTypewriter() {
+    const element = document.querySelector('.typewriter');
+    if (!element) {
+      return;
+    }
+
+    const texts = [
+      'RICHA CHADHA',
+      'Full Stack Software Developer.'
+    ];
+    let textIndex = 0;
+    let text = '';
+    let isDeleting = false;
+    let typingSpeed = 200; // Adjust typing speed (lower value = faster)
+    let deletingSpeed = 100; // Adjust deleting speed (lower value = faster)
+
+    function type() {
+      if (textIndex === texts.length) {
+        textIndex = 0;
+      }
+
+      text = isDeleting
+        ? texts[textIndex].substring(0, text.length - 1)
+        : texts[textIndex].substring(0, text.length + 1);
+
+      if (element) {
+        element.innerHTML = `<span class="wrap">${text}</span>`;
+      }
+
+      if (!isDeleting && text === texts[textIndex]) {
+        isDeleting = true;
+        typingSpeed = 2000; // Delay after completion
+      } else if (isDeleting && text === '') {
+        isDeleting = false;
+        textIndex++;
+        typingSpeed = 80; // Typing speed
+      }
+
+      setTimeout(type, isDeleting ? deletingSpeed : typingSpeed);
+    }
+
+    type();
+  }
 }
